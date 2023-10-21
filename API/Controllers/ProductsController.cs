e64553cf68c0b0ba653e9f562d360a86232c2cd1
unknown <bartlomiej.wyszecki2@gmail.com>
2023-10-21 22:14:45 +0200

    Products controller added

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext Context;
+        public ProductsController(StoreContext context)
+        {
+            this.Context = context;
+            
+        }
+    }
+}
