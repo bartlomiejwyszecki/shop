b0baf8ca35530d5cfa4651bda9d69aad85564591
unknown <bartlomiej.wyszecki2@gmail.com>
2023-12-03 19:13:24 +0100

    product details fetching from url param functionality added

--- shop-ui/src/app/features/product-details/ProductDetails.tsx
+++ shop-ui/src/app/features/product-details/ProductDetails.tsx
@@ -1,10 +1,29 @@
 import { Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Product } from "../../models/product.interface";
 
 export default function ProductDetailsPage() {
   const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  console.log(id);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5265/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
 
-  return <Typography variant="h2">Product details</Typography>;
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (!product) {
+    return <h3>Product not found</h3>;
+  }
+
+  return <Typography variant="h2">{product.name}</Typography>;
 }
