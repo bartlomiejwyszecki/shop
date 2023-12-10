48c96e0776de4ab07166201c21c876d30596b2cc
unknown <bartlomiej.wyszecki2@gmail.com>
2023-12-10 21:43:49 +0100

    test errors http added

--- shop-ui/src/app/api/testErrorsHttp.ts
+++ shop-ui/src/app/api/testErrorsHttp.ts
@@ -1,5 +1,9 @@
 import requests from "./httpClient";
 
 export const testErrorsHttp = {
-//   get
+  get400Error: () => requests.get("buggy/bad-request"),
+  get401Error: () => requests.get("buggy/unauthorized"),
+  get404Error: () => requests.get("buggy/not-found"),
+  get500Error: () => requests.get("buggy/server-error"),
+  getValidationError: () => requests.get("buggy/validation-error"),
 };
