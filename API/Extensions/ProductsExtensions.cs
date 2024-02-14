fdcddc15e6f25095db2357b49995a6a82f4b5c16
unknown <bartlomiej.wyszecki2@gmail.com>
2024-02-14 21:53:27 +0100

    search by query added

--- API/Extensions/ProductsExtensions.cs
+++ API/Extensions/ProductsExtensions.cs
@@ -11,7 +11,7 @@ namespace API.Extensions
         public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
         {
             if (string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p => p.Name);
-            
+
             query = orderBy switch
             {
                 "price" => query.OrderBy(p => p.Price),
@@ -20,6 +20,15 @@ namespace API.Extensions
             };
 
             return query;
+        }
+
+        public static IQueryable<Product> Search(this IQueryable<Product> query, string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm)) return query;
+
+            var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
+
+            return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
         }
     }
 }
