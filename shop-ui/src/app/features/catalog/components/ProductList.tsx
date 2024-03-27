403217398d1505180bcd6d760a3479598461dc2e
unknown <bartlomiej.wyszecki2@gmail.com>
2024-03-27 22:21:14 +0100

    Catalog products skeleton added as loader

--- shop-ui/src/app/features/catalog/components/ProductList.tsx
+++ shop-ui/src/app/features/catalog/components/ProductList.tsx
@@ -1,17 +1,25 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../../models/product.interface";
 import { ProductCard } from "./ProductCard";
+import { useAppSelector } from "../../../store/configureStore";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface ProductListProps {
   products: Product[];
 }
 
 export default function ProductList({ products }: ProductListProps) {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
+
   return (
     <Grid container spacing={3}>
       {products.map((product) => (
         <Grid key={product.id} item xs={12} sm={6} lg={4}>
-          <ProductCard key={product.id} product={product} />
+          {!productsLoaded ? (
+            <ProductCardSkeleton />
+          ) : (
+            <ProductCard key={product.id} product={product} />
+          )}
         </Grid>
       ))}
     </Grid>
